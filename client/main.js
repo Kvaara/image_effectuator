0fbad07fdb18d71de1fa8fd8a3654c5c7e17bb4d
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2022-02-12 17:47:48 +0200

    Started implementing the uploading process. In addition, I stylized and customized the page a little more. Added a sans serif font Poppins and a display font Righteous.

--- client/main.js
+++ client/main.js
@@ -1,1 +1,20 @@
-console.log('test');
+const initialize = () => {
+  const input = document.querySelector("#upload");
+
+  // FileReader will be used for transforming the binary file using Base64 encoding into a string
+  const fileReader = new FileReader();
+
+  fileReader.onloadend = () => {
+    const base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ""
+    );
+    console.log(base64);
+  };
+
+  input.addEventListener("change", () => {
+    fileReader.readAsDataURL(input.files[0]);
+  });
+};
+
+initialize();
