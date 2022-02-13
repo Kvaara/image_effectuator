3a8583c2f935db0107a4ea76aced2fa5a8ca9f3e
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2022-02-13 20:01:44 +0200

    Created a buffer using the Cursor struct. The img file will be transformed back into byte slices and inserted into this buffer. Cursor wraps the Vec<u8> and provides it the Seek implementation as well as Write and Read functionalities, which are needed by the write_to function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+use std::io::Cursor;
+
 use base64::decode;
 use image::load_from_memory;
+use image::ImageOutputFormat::Png;
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 
@@ -10,6 +13,14 @@ pub fn grayscale(encoded_file: &str) {
     let base64_to_vector = decode(encoded_file).unwrap();
     log(&"Image decoded".into());
 
-    let img = load_from_memory(&base64_to_vector).unwrap();
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
     log(&"Image loaded".into());
+
+    img = img.grayscale();
+    log(&"Image grayscale applied".into());
+
+    let mut buffer = Cursor::new(vec![]);
+
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"New image written".into());
 }
