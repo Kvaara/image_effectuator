fe7bff74b7521263da13b2a0bdaa4f275bee3a3a
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2022-02-13 20:33:46 +0200

    Encoded the buffer into base64 with the help of the function get_ref. The get_ref function is super useful because with it we can get the Cursor's underlying value, which is Vec<u8>. In addition, I used the format! macro to append the dataurl and base64 strings together. This is needed in the JS side.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
+use std::fmt::format;
 use std::io::Cursor;
 
-use base64::decode;
+use base64::{decode, encode};
 use image::load_from_memory;
 use image::ImageOutputFormat::Png;
 use wasm_bindgen::prelude::*;
 use web_sys::console::log_1 as log;
 
 #[wasm_bindgen]
-pub fn grayscale(encoded_file: &str) {
+pub fn grayscale(encoded_file: &str) -> String {
     log(&"Grayscale called".into());
 
     let base64_to_vector = decode(encoded_file).unwrap();
@@ -23,4 +24,11 @@ pub fn grayscale(encoded_file: &str) {
 
     img.write_to(&mut buffer, Png).unwrap();
     log(&"New image written".into());
+
+    let vector_to_base64 = encode(&buffer.get_ref());
+    log(&"New image buffer encoded to Base64".into());
+
+    let data_url = format!("data:image/png;base64,{}", vector_to_base64);
+
+    return data_url;
 }
