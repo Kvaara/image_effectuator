aff5f81700bbbfd33a9b5ca699d036a09361dd20
Jesse Karnavaara <karnavaara.jesse@gmail.com>
2022-02-19 16:55:57 +0200

    Users can now choose a processing effect between three radio button options: grayscale, brighten or blur. Selecting grayscale will grayscale the image and so on. This is a pretty basic way mainly because the user can't really change or choose the precise settings of the image manipulation. I'll need to implement this too so that users can set their own blur, grayscale or brightening settings.

--- client/main.js
+++ client/main.js
@@ -9,14 +9,17 @@ const browseFilesBtn = document.querySelector("#browse-files");
 const dropZone = document.querySelector("#drop-zone");
 const dropZoneContent = document.querySelector("#content-wrapper");
 const loadingIndicator = document.querySelector("#loading-indicator");
+const radioBtns = document.querySelectorAll(".custom-radio");
 
 // FileReader will be used for transforming the binary file using Base64 encoding into a string
 const fileReader = new FileReader();
 
+let rustApp = null;
+
 const initialize = async () => {
+  makeRadioButtonsDynamic();
   listenToFileDrags();
 
-  let rustApp = null;
   try {
     const workerInstance = new Worker(new URL("./worker.js", import.meta.url));
     const Wrapper = wrap(workerInstance);
@@ -26,23 +29,39 @@ const initialize = async () => {
   }
 
   fileReader.onloadend = async () => {
-    addDisabledStyles();
+    disableDragZone();
+    disableRadioButtons();
 
     const base64 = fileReader.result.replace(
       /^data:image\/(png|jpeg|jpg);base64,/,
       ""
     );
-    let imgDataUrl = await rustApp.blur(base64);
+    let imgDataUrl = await getChosenProcessingMethod(base64);
     oldImage.src = fileReader.result;
     newImage.src = imgDataUrl;
     imagesWrapper.classList.remove("hidden");
 
-    removeDisabledStyles();
-    removeDragStyles();
+    enableDragZone();
+    enableRadioButtons();
+    removeOnDraggingStyles();
   };
 
   input.addEventListener("change", () => {
     fileReader.readAsDataURL(input.files[0]);
+  });
+};
+
+const makeRadioButtonsDynamic = () => {
+  radioBtns.forEach((btn) => {
+    const input = btn.firstElementChild;
+    input.addEventListener("change", () => {
+      removeRadioBtnsSelectedStyles();
+      if (input.checked) {
+        addRadioBtnSelectedStyles(btn);
+      } else {
+        removeRadioBtnSelectedStyles(btn);
+      }
+    });
   });
 };
 
@@ -71,40 +90,84 @@ const listenToFileDrags = () => {
   });
   dropZone.addEventListener("dragleave", (event) => {
     event.preventDefault();
-    removeDragStyles();
+    removeOnDraggingStyles();
   });
   dropZone.addEventListener("dragend", (event) => {
     event.preventDefault();
-    removeDragStyles();
+    removeOnDraggingStyles();
   });
   dropZone.addEventListener("mouseleave", (event) => {
     event.preventDefault();
-    removeDragStyles();
+    removeOnDraggingStyles();
   });
 };
 
-const addDisabledStyles = () => {
+const disableDragZone = () => {
   dropZoneContent.classList.add("opacity-10");
   dropZone.classList.add("pointer-events-none");
   loadingIndicator.classList.remove("hidden");
 };
 
-const removeDisabledStyles = () => {
+const disableRadioButtons = () => {
+  radioBtns.forEach((btn) => {
+    btn.classList.add("opacity-40", "pointer-events-none");
+  });
+};
+
+const getChosenProcessingMethod = async (base64) => {
+  const checkedRadioBtn = document.querySelector("input[type=radio]:checked");
+  const processingMethod = checkedRadioBtn.id;
+
+  switch (processingMethod) {
+    case "grayscale":
+      return await rustApp.grayscale(base64);
+    case "brighten":
+      return await rustApp.brighten(base64);
+    case "blur":
+      return await rustApp.blur(base64);
+    default:
+      break;
+  }
+};
+
+const enableDragZone = () => {
   dropZoneContent.classList.remove("opacity-10");
   dropZone.classList.remove("pointer-events-none");
   loadingIndicator.classList.add("hidden");
+};
+
+const enableRadioButtons = () => {
+  radioBtns.forEach((btn) => {
+    btn.classList.remove("opacity-40", "pointer-events-none");
+  });
+};
+
+const removeOnDraggingStyles = () => {
+  browseFilesBtn.classList.remove("pointer-events-none");
+  dropZoneContent.classList.remove("pointer-events-none");
+  dropZone.classList.remove("bg-gray-300", "border-gray-500");
+};
+
+const removeRadioBtnsSelectedStyles = () => {
+  radioBtns.forEach((btn) => {
+    removeRadioBtnSelectedStyles(btn);
+  });
+};
+
+const addRadioBtnSelectedStyles = (btn) => {
+  btn.classList.remove("tracking-wider");
+  btn.classList.add("tracking-normal", "bg-blue-50");
+};
+
+const removeRadioBtnSelectedStyles = (btn) => {
+  btn.classList.add("tracking-wider");
+  btn.classList.remove("tracking-normal", "bg-blue-50");
 };
 
 const addDragStyles = () => {
   browseFilesBtn.classList.add("pointer-events-none");
   dropZoneContent.classList.add("pointer-events-none");
   dropZone.classList.add("bg-gray-300", "border-gray-500");
-};
-
-const removeDragStyles = () => {
-  browseFilesBtn.classList.remove("pointer-events-none");
-  dropZoneContent.classList.remove("pointer-events-none");
-  dropZone.classList.remove("bg-gray-300", "border-gray-500");
 };
 
 initialize();
